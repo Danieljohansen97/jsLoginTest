f106698612b71370eb11b1b06f09a629eb3dc3a3
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-09-17 14:44:03 +0200

    Added functionality for registering an user

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 var users = [
-    admin = {
+    {
         username: "admin",
         password: "password"
     }
 ]
 
+// clickevent for registering a user
+var registerBtn = document.getElementById("registerBtn").addEventListener("click",
 function register(username, password){
+    username = document.getElementById("inputUsername").value;
+    password = document.getElementById("inputPassword").value;
     this.username = username;
     this.password = password;
-    users.push(this.username = {
-        username: username,
-        password: password
-    })
+    
+    // logic to check if user already exists
+    let exists = false;
+    for(i = 0; i < users.length; i++){
+        if(users[i].username === username){
+            exists = true;
+            alert("Username is already taken!");
+        } 
+    }
+    if(!exists){
+        users.push({
+            username: username,
+            password: password
+        })
+        alert("User registered with username: " + username);
+    }
+})
+
+function printUsers(){
+    for(i = 0; i < users.length; i++){
+        console.log(users[i]);
+    }
 }
+
+var loginBtn = document.getElementById("loginBtn");
